a8459de82dadf7e639502610c567cea16ca3e943
Jasti9 <satyajasi9@gmail.com>
2019-10-04 16:01:04 +0530

    add

---
+++ Star1.java
@@ -0,0 +1,35 @@
+class Star1
+{
+void star1()
+{
+for(int i=0;i<=5;i++)
+{
+for(int j=0;j<=5;j++)
+{
+System.out.print(i);
+}
+System.out.println(" ");
+}
+}
+void star2()
+{
+System.out.println(" ");
+for(int i=0;i<=5;i++)
+{
+for(int j=0;j<=5;j++)
+{
+System.out.print(j);
+}
+System.out.println(" ");
+}
+}
+
+public static void main(String[]args)
+{
+Star1 s=new Star1();
+s.star1();
+s.star2();
+}
+}
+
+
