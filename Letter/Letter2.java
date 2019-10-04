1ff197a7a5a0cc3f2512446576e6cfd900d3473f
Jasti9 <satyajasi9@gmail.com>
2019-10-04 20:39:27 +0530

    add

---
+++ Letter/Letter2.java
@@ -0,0 +1,38 @@
+class Letter2
+{
+
+void letter2()
+{
+for(char a='A';a<='E';a++)
+{
+for(char b='A';b<=a;b++)
+{
+System.out.print(b);
+}
+System.out.println(" ");
+}
+}
+
+void letter3()
+{
+System.out.println(" ");
+for(char a='A';a<='E';a++)
+{
+for(char b='A';b<=a;b++)
+{
+System.out.print(a);
+}
+System.out.println(" ");
+}
+}
+
+
+public static void main(String[]args)
+{
+Letter2 l=new Letter2();
+l.letter2();
+l.letter3();
+}
+}
+
+
