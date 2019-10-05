894d25748195acfceb362fe2548a841f78c3fd13
Jasti9 <satyajasi9@gmail.com>
2019-10-05 19:41:29 +0530

    add

---
+++ Star/Sample.java
@@ -0,0 +1,21 @@
+class Sample
+{
+
+void sample()
+{
+for(int i=0;i<=4;i++)
+{
+for(int j=0;j<=4;j++)
+{
+System.out.print("🌹");
+}
+System.out.println(" ");
+}
+}
+
+public static void main(String[]args)
+{
+Sample s=new Sample();
+s.sample();
+}
+}
