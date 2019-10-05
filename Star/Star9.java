e00cf30ee967973f6228d9550df47db069397b9d
Jasti9 <satyajasi9@gmail.com>
2019-10-05 16:28:39 +0530

    add9

---
+++ Star/Star9.java
@@ -0,0 +1,29 @@
+class Star9
+{
+
+void star9()
+{
+for(int i=0;i<=4;i++)
+{
+for(int j=0;j<=4;j++)
+{
+if( (i+j)==0 || (i+j)==8 || (j-i)==4 || (i==4)&&(j==0) || (i==2)&&(j==2))
+{
+System.out.print("*");
+}
+else
+{
+System.out.print(" ");
+}
+}
+System.out.println(" ");
+}
+}
+
+public static void main(String[]args)
+{
+Star9 s=new Star9();
+s.star9();
+}
+}
+
