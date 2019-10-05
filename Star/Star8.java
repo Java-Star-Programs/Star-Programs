3a906f9598d234153244c78abeecf05d20f1b323
Jasti9 <satyajasi9@gmail.com>
2019-10-05 16:20:16 +0530

    add

---
+++ Star/Star8.java
@@ -0,0 +1,40 @@
+class Star8
+{
+
+void star8()
+{
+for(int i=0;i<=4;i++)
+{
+for(int j=0;j<=4;j++)
+{
+if( (i+j)==4 || (i+j)==5 || (i+j)==6 || (i+j)==7 || (i+j)==8)
+{
+System.out.print("*");
+}
+else
+{
+System.out.print(" ");
+}
+}
+System.out.println(" ");
+}
+}
+
+public static void main(String[]args)
+{
+Star8 s=new Star8();
+s.star8();
+}
+}
+
+O/P:
+
+    * 
+   ** 
+  *** 
+ **** 
+***** 
+
+
+
+ 
