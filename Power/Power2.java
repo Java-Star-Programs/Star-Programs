4f354f0d7bfb311f96ba7c611056c4fc3b83b55d
Jasti9 <satyajasi9@gmail.com>
2019-10-06 10:20:55 +0530

    add

---
+++ Power/Power2.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+class Power2
+{
+
+void power2()
+{
+int b=0;
+
+System.out.print("Up to Numbers: ");
+Scanner s=new Scanner(System.in);
+int n=s.nextInt();
+
+for(int i=1;i<=n;i++)
+{
+b=(int)Math.pow(i,3);
+System.out.print(b+"\t"+"+  ");
+}
+}
+
+public static void main(String[]args)
+{
+Power2 p=new Power2();
+p.power2();
+}
+}
+
+O/P:
+
+Up to Numbers: 10
+1	+  8	+  27	+  64	+  125	+  216	+  343	+  512	+  729	+  1000	+  
+
+
+
+
