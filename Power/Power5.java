dfbeeba6cfa4e231cf7a3a7144d67dabcf7767a9
Jasti9 <satyajasi9@gmail.com>
2019-10-06 11:21:49 +0530

    add

--- Power/Power5.java
+++ Power/Power5.java
@@ -1,18 +1,18 @@
 import java.util.Scanner;
 
-//Squres of Odd Numbers
+//Squres of Even Numbers
 
-class Power4
+class Power5
 {
 
-void power4()
+void power5()
 {
 int c=0;
 System.out.print("Up to Numbers: ");
 Scanner sc=new Scanner(System.in);
 int n=sc.nextInt();
 
-for(int i=1;i<=n;i=i+2)
+for(int i=2;i<=n;i=i+2)
 {
 c=(int)Math.pow(i,2);
 System.out.print(c+" \t");
@@ -21,15 +21,12 @@ System.out.print(c+" \t");
 
 public static void main(String[]args)
 {
-Power4 p=new Power4();
-p.power4();
+Power5 p=new Power5();
+p.power5();
 }
 }
 
-O/P:
-
-Up to Numbers: 20
-1 	9 	25 	49 	81 	121 	169 	225 	289 	361 	
-
+Up to Numbers: 10
+4 	16 	36 	64 	100 
 
 
