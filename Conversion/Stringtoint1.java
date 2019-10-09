05dd7148867785f241f5954e1349892f7613550f
Jasti9 <satyajasi9@gmail.com>
2019-10-09 11:30:51 +0530

    add

---
+++ Conversion/Stringtoint1.java
@@ -0,0 +1,20 @@
+class Stringtoint1
+{
+// String to Int Conversion
+void display()
+{
+String s="100";
+int n=Integer.parseInt(s); 
+System.out.print("String to Int Conversion: "+n);
+}
+
+public static void main(String[]args)
+{
+Stringtoint1 s1=new Stringtoint1();
+s1.display();
+}
+}
+
+O/P:
+
+String to Int Conversion: 100
