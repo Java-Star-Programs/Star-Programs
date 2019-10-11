28108d4b2db855abd121afed5f40563995fc012a
Jasti9 <satyajasi9@gmail.com>
2019-10-11 11:25:02 +0530

    add

---
+++ Conversion/Stringtoint2.java
@@ -0,0 +1,14 @@
+class Stringtoint2
+{
+
+//String to Int Conversion
+
+void display()
+{
+String s="4000";
+
+int n=Integer.valueof(s);
+System.out.print("String to Int Conversion: "+s);
+}
+
+public static void main
