f2b6fbe9c0f8fc5f7400e0710d49f730c389efa5
Satyakrishna Jasti <satyajasti9@gmail.com>
2019-10-17 15:30:27 +0530

    Create Stringlist.java

---
+++ String/Stringlist.java
@@ -0,0 +1,34 @@
+
+public class Stringlist {
+
+	
+	
+	void display()
+	{
+		String name1="Java return keyword is used to complete the execution of a method. The return followed by the appropriate value that is returned to the caller. This value depends on the method return type like int method always return an integer value.";
+		System.out.println("Length of the String  "+name1.length());
+		System.out.println(" ");
+		System.out.println("Uppercase  "+name1.toUpperCase());
+		System.out.println(" ");
+		System.out.println("lowercase  "+name1.toLowerCase());
+		System.out.println(" ");
+		System.out.println("Starts with string   "+name1.startsWith("J"));
+		System.out.println(" ");
+		System.out.println("Ends with String   "+name1.endsWith("e"));
+		System.out.println(" ");
+		System.out.println("Char at method of string  "+name1.charAt(100));
+		System.out.println(" ");
+		System.out.println("Index of the String  "+name1.indexOf("e"));
+	
+	}
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Stringlist s=new Stringlist();
+		s.display();
+		
+	}
+
+}
