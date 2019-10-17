2ac27dc10f2bf760d89f16bba8ebb30c9ff5d3b6
Jasti9 <satyajasi9@gmail.com>
2019-10-17 11:32:37 +0530

    add

---
+++ Array/Array2.java
@@ -0,0 +1,16 @@
+class Array2
+{
+void display()
+{
+int a[]=new int[3];
+System.out.println(a.getClass().getName());
+}
+
+
+public static void main(String[]args)
+{
+Array2 a=new Array2();
+a.display();
+}
+}
+
