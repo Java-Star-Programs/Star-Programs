d18e233eeb6b33a330ce7656126d2e54d2fc4e25
Jasti9 <satyajasi9@gmail.com>
2019-10-17 11:37:52 +0530

    add

---
+++ Array/Array3.java
@@ -0,0 +1,15 @@
+class Array3
+{
+void display()
+{
+double []d=new double[4];
+System.out.println(d.getClass().getName());
+}
+
+public static void main(String[]args)
+{
+Array3 a=new Array3();
+a.display();
+}
+}
+
